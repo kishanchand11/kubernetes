37e74e2c83dead29b4812e4409a8c6ddc8f5f7d4
kishanchand11 <82415668+kishanchand11@users.noreply.github.com>
2024-04-03 17:15:38 +0530

    Update kcfg.sh

--- kcfg.sh
+++ kcfg.sh
@@ -3,3 +3,6 @@ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 #copy curl -sSL https://github.com/kishanchand11/kubernetes/raw/master/kcfg.sh | tee kcfg.sh | sudo bash
+
+# curl https://raw.githubusercontent.com/projectcalico/calico/v3.27.2/manifests/calico.yaml -O
+# kubectl apply -f calico.yaml
