db3350122bc8b5e593b4ca327fbf3953803cd68b
kishanchand11 <82415668+kishanchand11@users.noreply.github.com>
2024-04-03 16:31:39 +0530

    Update kcfg.sh

--- kcfg.sh
+++ kcfg.sh
@@ -2,4 +2,4 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-#copy curl -sSL https://github.com/kishanchand11/kubernetes/master/kcfg.sh | tee kcfg.sh | sudo bash
+#copy curl -sSL https://github.com/kishanchand11/kubernetes/raw/master/kcfg.sh | tee kcfg.sh | sudo bash
