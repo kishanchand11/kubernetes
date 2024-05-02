3a2445379a253d3e292978b4064a6b393f4ee82b
kishanchand11 <82415668+kishanchand11@users.noreply.github.com>
2024-05-02 16:29:23 +0530

    Update kpurge.sh

--- kpurge.sh
+++ kpurge.sh
@@ -2,4 +2,4 @@ kubeadm reset
 sudo apt-get purge kubeadm kubectl kubelet kubernetes-cni kube*   
 sudo apt-get autoremove  
 
-#curl -sSL 
+#curl -sSL https://github.com/kishanchand11/kubernetes/raw/master/kpurge.sh | sudo tee kpurge.sh | sudo bash
